21311c0371bd62f108f05e5566b8a1bdd8847700
Gonzalo Bertinat <gonzalo_bertinat_98@hotmail.com>
2022-05-29 19:49:23 -0300

    Se elimina componente App redundante y se migra contenido del sitio a index.js + ajuste de ícono en index.html

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { CartContextProvider } from './store/CartContext';
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './pages/ItemListContainer/ItemListContainer';
+import Cart from './pages/Cart/Cart';
+import ItemDetailContainer from './pages/ItemDetailContainer/ItemDetailContainer';
+import Checkout from './pages/Checkout/Checkout';
+import NotFound from './pages/NotFound/NotFound';
+import Footer from './components/Footer/Footer';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <CartContextProvider>
-        <App />
+        <div className="App">
+          <header>
+            <NavBar />
+          </header>
+          <main>
+            <Routes>
+              <Route path='/' element={<ItemListContainer />} />
+              <Route path='/cart' element={<Cart />} />
+              <Route path='/category/:categoryId' element={<ItemListContainer />} />
+              <Route path='/item/:id' element={<ItemDetailContainer />} />
+              <Route path='/checkout' element={<Checkout />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </main>
+          <footer>
+            <Footer />
+          </footer>
+        </div>
       </CartContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
